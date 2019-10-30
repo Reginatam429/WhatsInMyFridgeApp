9b3d7247553a2a0b7d07782389b4df24738851a5
Regina <reginatam429@gmail.com>
2019-10-29 18:03:30 -0700

    updated routes for testing

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -14,6 +15,19 @@ app.get('/', (request, response) => {
     response.json({ info: 'Setting up your fridge systems in: Node.js, Express, and Postgres API' })
 })
 
+app.get('/users/:id', db.getUser)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.get('/users/:id/ingredients', db.getIngredient)
+app.post('/users/:id/ingredients', db.createIngredient)
+app.delete('/users/:id/ingredients', db.deleteIngredient)
+
+app.get('/users/:id/recipes', db.getFavRecipe)
+app.post('/users/:id/recipes', db.createFavRecipe)
+app.put('/users/:id/recipes', db.updateFavRecipe)
+
 app.listen(port, () => {
-   console.log(`Refridgerator is running on port ${port}.`)
-})
+    console.log(`Refridgerator is running on port ${port}.`)
+ })
