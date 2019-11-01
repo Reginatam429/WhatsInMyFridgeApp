244271971bb7fa1003fbf6a8d2f6cf9ecfdcaed1
Regina <reginatam429@gmail.com>
2019-10-31 19:37:09 -0700

    added heroku dev pool and commented out local dev pool

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 5000
 const db = require('./queries')
 
 app.use(bodyParser.json())
