0fadac39b2a3667179a36d96499226235200e534
Regina <reginatam429@gmail.com>
2019-11-05 13:35:59 -0800

    nav an hp fixed

---
+++ client/src/components/Hompage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>This is my homepage</h1>
+        </div>
+       
+        
+    )
+}
+
+export default Homepage;
