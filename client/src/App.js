a0991e5cf45fec12a7d9491c59be3621a913e6a1
Regina <reginatam429@gmail.com>
2019-11-05 13:35:59 -0800

    nav an hp fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import Navbar from './components/navbar/Navbar'
 
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//import "bootstrap/dist/css/bootstrap.min.css"
+
+//Navbar
+//import Navbar from './components/Navbar'
+
+//Homepage
+import Homepage from './components/Hompage';
 
 function App() {
   return (
+    <body>
+
+    
     <div className="App">
-      <Navbar />
-      <h1>Hello</h1>
+       <Router>
+      
+        <div className="container">
+          {/* <Navbar /> */}
+          <br/>
+            {/*Homepage Route*/}
+            <Route path="/" exact component={Homepage} />
+
+               
+        </div>
+      </Router>
      
     </div>
+    </body>
   );
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
     return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light mb-5">
             
             <span className="navbar-brand.mb-0.h1.mx-auto">
-            <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="logo" />
+            <img src={logo} width="50" height="30" class="d-inline-block align-top" alt="logo" />
            
             What'sInMyFridge
 
