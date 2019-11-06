1106ec5e0927f15c0aebbf229c21df326872256a
Regina <reginatam429@gmail.com>
2019-11-06 08:44:48 -0800

    updated Navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,16 +4,18 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light mb-5">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5">
             
+           {/* logo */}
             <span className="navbar-brand.mb-0.h1.mx-auto">
             <NavLink to="/">
-                <img src={logo} width="80" height="30" className="d-inline-block align-top" alt="logo" />
+                <img src={logo} width="90" height="35" className="d-inline-block align-top" alt="logo" />
             </NavLink>
-           
-            <NavLink to="/">WhatsInMyFridge</NavLink>
 
-           
+           {/* App Name */}
+            <NavLink to="/"><span className="navbar-brand mb-0 h1">WhatsInMyFridge</span></NavLink>
+
+           {/* Login/SignUp Button */}
             </span>
             <form className="form-inline">
             <NavLink to="/login">
