0df357dafd90f4335759b40f88d3540ff57bdf9e
Regina <reginatam429@gmail.com>
2019-11-06 13:40:28 -0800

    editing navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
-import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //import "bootstrap/dist/css/bootstrap.min.css"
-
+import './App.css';
 
 //Homepage
 import Homepage from './components/Hompage';
