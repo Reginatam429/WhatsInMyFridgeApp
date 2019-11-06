6d39a071e6d5cc9f307b45b9b59a0e12e7a0c5e3
Regina <reginatam429@gmail.com>
2019-11-06 11:50:51 -0800

    fixed up hpbanner with jumbotron and added auth0 env

--- client/src/components/Hompage.js
+++ client/src/components/Hompage.js
@@ -3,19 +3,19 @@ import '../App.css';
 import hpbanner1 from '../assets/hpbanner1.png';
 
 
-const divStyle = {
-    width: '100%',
-    height: '500px',
-    backgroundImage: `url(${hpbanner1})`,
-    backgroundSize: 'cover'
-  };
+// const divStyle = {
+//     width: '100%',
+//     height: '500px',
+//     backgroundImage: `url(${hpbanner1})`,
+//     backgroundSize: 'cover'
+//   };
 
  const Homepage = () => {
     
     return (
     <div className='hp'>
-        <div style={divStyle}>
-            {/* <img className='hpbanner' src={ hpbanner } width="100%" height="100%" margin="0" alt='homepage banner'/> */}
+        <div className='jumbotron '>
+            {/* <img className='container' src={ hpbanner1 } width="100%" height="100%" margin="0" backgroundSize="cover" alt='homepage banner'/> */}
         </div>
        <div>
            <h1>This is my homepage</h1>
