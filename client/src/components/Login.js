f8ff8418bbe2bd5d471773b3a547ee4cfc8439f7
Regina <reginatam429@gmail.com>
2019-11-05 17:20:41 -0800

    added login/signup navigation

---
+++ client/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>This is my Login/Sign Up</h1>
+        </div>
+       
+        
+    )
+}
+
+export default Login;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light mb-5">
             
             <span className="navbar-brand.mb-0.h1.mx-auto">
-            <img src={logo} width="50" height="30" class="d-inline-block align-top" alt="logo" />
+            <NavLink to="/">
+                <img src={logo} width="80" height="30" className="d-inline-block align-top" alt="logo" />
+            </NavLink>
            
-            What'sInMyFridge
+            <NavLink to="/">WhatsInMyFridge</NavLink>
 
            
             </span>
-            <form class="form-inline">
-                <button class="btn btn-sm btn-outline-success" type="button">Login / Sign Up</button>
+            <form className="form-inline">
+            <NavLink to="/login">
+                <button className="btn btn-sm btn-outline-success" type="button">
+                    Login / Sign Up
+                </button>
+            </NavLink>
             </form>
         </nav>
     )
