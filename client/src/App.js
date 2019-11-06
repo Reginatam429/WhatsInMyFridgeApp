7d1cb09dccaaebf09599c578f699f79c2ef2ad99
Regina <reginatam429@gmail.com>
2019-11-05 17:20:41 -0800

    added login/signup navigation

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,30 @@ import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 //import "bootstrap/dist/css/bootstrap.min.css"
 
-//Navbar
-//import Navbar from './components/Navbar'
 
 //Homepage
 import Homepage from './components/Hompage';
+//Login/SignUp
+import Login from './components/Login';
 
 function App() {
   return (
-    <body>
-
     
     <div className="App">
        <Router>
       
         <div className="container">
-          {/* <Navbar /> */}
-          <br/>
             {/*Homepage Route*/}
             <Route path="/" exact component={Homepage} />
 
+            {/*Login/Signup Route*/}
+            <Route path="/login" exact component={Login} />
                
         </div>
       </Router>
      
     </div>
-    </body>
+
   );
 }
 
