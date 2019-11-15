703c4488512d882f4c8707875cc64321a42dc875
Regina <reginatam429@gmail.com>
2019-11-14 16:54:09 -0800

    added rotating banner messages

--- client/src/components/Hpbanner.js
+++ client/src/components/Hpbanner.js
@@ -20,16 +20,29 @@ import hpbanner9 from '../assets/hpbanner9.png';
     hpbanner9
     ];
 
+let phrases = [
+    "REDUCE FOOD WASTE.",
+    "CHECK YOUR FRIDGE.",
+    "FEELING HUNGRY?",
+    "LEARN TO COOK.",
+    "SAVE MONEY.",
+    "AVOID SPOILAGE",
+    "WE DO HAVE FOOD AT HOME.",
+    "FIND A NEW RECIPE"
+]
+
  
  const Hpbanner = () => {
 
     const imageUrl = images[Math.floor(Math.random() * images.length)];
+
+    const bannerMessage = phrases[Math.floor(Math.random() * phrases.length)];
     
     return (
     <div className='hp'>
         <div className='banner' style={{backgroundImage: `url(${imageUrl})` }} >
             <div className="banner-square">
-                <h1 className="banner-phrase">Reduce Food Waste.</h1> <br/>
+                <h1 className="banner-phrase">{bannerMessage}</h1> <br/>
                 <h5 className="banner-phrase2">Use What's In Your Fridge.</h5>
             </div>
         </div>
