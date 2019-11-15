29c540ee39d880be0ba7aa3ef89b76f397b60723
Regina <reginatam429@gmail.com>
2019-11-14 16:40:00 -0800

    fixed reloading banners

--- client/src/components/Hpbanner.js
+++ client/src/components/Hpbanner.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import '../App.css';
-// import hpbanner2 from '../assets/hpbanner2.png';
-// import hpbanner3 from '../assets/hpbanner3.png';
-// import hpbanner4 from '../assets/hpbanner4.png';
-// import hpbanner5 from '../assets/hpbanner5.png';
-// import hpbanner6 from '../assets/hpbanner6.png';
-// import hpbanner7 from '../assets/hpbanner7.png';
-// import hpbanner8 from '../assets/hpbanner8.png';
-// import hpbanner9 from '../assets/hpbanner9.png';
+import hpbanner2 from '../assets/hpbanner2.png';
+import hpbanner3 from '../assets/hpbanner3.png';
+import hpbanner4 from '../assets/hpbanner4.png';
+import hpbanner5 from '../assets/hpbanner5.png';
+import hpbanner6 from '../assets/hpbanner6.png';
+import hpbanner7 from '../assets/hpbanner7.png';
+import hpbanner8 from '../assets/hpbanner8.png';
+import hpbanner9 from '../assets/hpbanner9.png';
 
  let images = [
-     "../assets/hpbanner2.png",
-     "../assets/hpbanner3.png",
-     "../assets/hpbanner4.png"
+    hpbanner2,
+    hpbanner3,
+    hpbanner4,
+    hpbanner5,
+    hpbanner6,
+    hpbanner7,
+    hpbanner8,
+    hpbanner9
     ];
 
-//  $('background-image:"url(' + images[Math.floor(Math.random() * images.length)] + '")>').appendTo('#jumbotron');
-//let background = document.querySelector('.jumbotron').backgroundImage;
-//document.getElementById('jumbotron').style.backgroundImage=`url(`+images[Math.floor(Math.random() * images.length)]+`)`; 
  
  const Hpbanner = () => {
 
+    const imageUrl = images[Math.floor(Math.random() * images.length)];
+    
     return (
     <div className='hp'>
-        <div className='jumbotron' >
+        <div className='banner' style={{backgroundImage: `url(${imageUrl})` }} >
             <div className="banner-square">
                 <h1 className="banner-phrase">Reduce Food Waste.</h1> <br/>
                 <h5 className="banner-phrase2">Use What's In Your Fridge.</h5>
-                {/* <img className='container' src={ hpbanner1 } width="100%" height="100%" margin="0" backgroundSize="cover" alt='homepage banner'/> */}
             </div>
         </div>
     </div>   
